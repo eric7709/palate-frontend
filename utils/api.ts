57574a3469a36a24b8e08ef643007bf41bbe49a0
Eric <emekaeric12@gmail.com>
2026-05-27 21:08:13 +0100

    standardize API instance with environment variables

--- utils/api.ts
+++ utils/api.ts
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
-// Replace with your laptop's IP address found via 'hostname -I'
-// const BASE_URL = 'http://localhost:8080/api/palate';
-const BASE_URL = 'https://palate-backend.onrender.com/api/palate';
 export const api = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: import.meta.env.VITE_API_URL, 
+  headers: { 'Content-Type': 'application/json' },
 });
