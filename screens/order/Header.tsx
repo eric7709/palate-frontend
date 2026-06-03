ad86ab42301e905ede5d59b11cf588fd83a1938f
Eric <emekaeric12@gmail.com>
2026-06-03 13:37:08 +0100

    Changed filter and order button on the admin order page

--- screens/order/Header.tsx
+++ screens/order/Header.tsx
@@ -3,17 +3,16 @@ import PageTitle from '@/ui/PageTitle'
 import { Filter, RefreshCcw } from 'lucide-react'
 
 export default function Header() {
-    const {resetFilters} =useOrderStore()
+    const { resetFilters } = useOrderStore()
     return (
         <div className='flex items-center gap-2'>
             <PageTitle subTitle='Manage your restaurant orders' title='Orders ' />
-            <div onClick={resetFilters}  className="flex ml-auto cursor-pointer duration-300 active:scale-90 items-center gap-2 text-white font-medium shadow  px-3 py-1.75 text-[13px] border-blue-500/30 border bg-amber-500 rounded-3xl
-            ">
+            <div onClick={resetFilters} className="flex ml-auto cursor-pointer duration-300 active:scale-90 items-center gap-2 text-white font-medium shadow px-3 py-1.75 text-[13px] border-white border bg-slate-600 rounded-3xl">
                 <RefreshCcw size={11} strokeWidth={3} />
                 <p>Refresh</p>
             </div>
-            <div onClick={resetFilters}  className="flex cursor-pointer duration-300 active:scale-90 items-center gap-2 text-white font-medium shadow  px-3 py-1.75 text-[13px] border-blue-500/30 border bg-green-500 rounded-3xl
-            ">
+
+            <div onClick={resetFilters} className="flex cursor-pointer duration-300 active:scale-90 items-center gap-2 text-white font-medium shadow px-3 py-1.75 text-[13px] border-white border bg-indigo-500 rounded-3xl">
                 <Filter size={11} strokeWidth={3} />
                 <p>Filter</p>
             </div>
